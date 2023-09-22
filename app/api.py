d009ee41bbceb3b86cbf8f3ed243b5229d50da1e
Alina <105233284+Alinamadness@users.noreply.github.com>
2023-09-22 22:31:34 +0300

    frontend...

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 import json
 
-from flask import Flask, request, render_template
-
+# from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from inference import get_recommendations
 
 
@@ -9,8 +9,29 @@ app = Flask(__name__)
 
 
 @app.route('/auth')
-def login():
+def log():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Dummy user validation
+        if username == 'admin' and password == 'password':
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 @app.route('/index')
